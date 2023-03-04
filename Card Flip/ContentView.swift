c516bd06be9252e82ce78540e7ebf0438b0ef419
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-04 21:37:29 +0700

    add card flip

--- Card Flip/ContentView.swift
+++ Card Flip/ContentView.swift
@@ -7,15 +7,85 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CardFront : View {
+    let width : CGFloat
+    let height : CGFloat
+    @Binding var degree: Double
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+                .frame(width: width, height: height)
+                .shadow(color: .gray, radius: 2, x: 0, y: 0)
+
+            Image(systemName: "suit.club.fill")
+                .resizable()
+                .frame(width: 80, height: 80)
+                .foregroundColor(.red)
+
         }
-        .padding()
+        .rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct CardBack : View {
+    let width : CGFloat
+    let height : CGFloat
+    @Binding var degree: Double
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.blue.opacity(0.7), lineWidth: 3)
+                .frame(width: width, height: height)
+
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.blue.opacity(0.2))
+                .frame(width: width, height: height)
+                .shadow(color: .gray, radius: 2, x: 0, y: 0)
+        }
+        .rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct ContentView: View {
+    //MARK: Variables
+    @State var backDegree = 0.0
+    @State var frontDegree = -90.0
+    @State var isFlipped = false
+
+    let width : CGFloat = 200
+    let height : CGFloat = 250
+    let durationAndDelay : CGFloat = 0.3
+
+    //MARK: Flip Card Function
+    func flipCard () {
+        isFlipped = !isFlipped
+        if isFlipped {
+            withAnimation(.linear(duration: durationAndDelay)) {
+                backDegree = 90
+            }
+            withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)){
+                frontDegree = 0
+            }
+        } else {
+            withAnimation(.linear(duration: durationAndDelay)) {
+                frontDegree = -90
+            }
+            withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)){
+                backDegree = 0
+            }
+        }
+    }
+    //MARK: View Body
+    var body: some View {
+        ZStack {
+            CardFront(width: width, height: height, degree: $frontDegree)
+            CardBack(width: width, height: height, degree: $backDegree)
+        }.onTapGesture {
+            flipCard ()
+        }
     }
 }
 

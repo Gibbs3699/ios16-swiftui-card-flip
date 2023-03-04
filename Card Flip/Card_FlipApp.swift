a9aff0c46501d4889b876be45268a2ca676db444
Gibbs3699 <gift_kamonpan@hotmail.com>
2023-03-04 16:38:21 +0700

    init project

---
+++ Card Flip/Card_FlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_FlipApp.swift
+//  Card Flip
+//
+//  Created by TheGIZzz on 4/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Card_FlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
